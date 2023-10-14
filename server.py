44d06a161e6a7eee0912b0ff583d9b90241d30b3
abdelrahman-toni <81957817+abdelrahman-toni@users.noreply.github.com>
2023-10-14 19:29:27 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+port=5666
+server_socket.bind(('127.0.0.1',80))
+server_socket.listen(5666)
+while True :
+            client,add = server_socket.accept(); 
+            while(sentence!="CLOSE SOCKET"):
+                    sentence= client.recv(1024).decode() 
+                    print("Client :               ",sentence)
+                    Capital_Sentence = sentence.upper()
+                    client.send(Capital_Sentence.encode()) 
+                    response_byt=Capital_Sentence.encode()
+                    client.send(response_byt)
+            client.close()        
+
+      
+                
+                    
+
+
+                       
